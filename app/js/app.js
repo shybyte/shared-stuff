bae5e64b25a28dfd0f9b29185d5393b4482eff6a
shybyte <ein@volloeko.de>
2012-04-08 12:31:02 +0200

    Refactoring

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/mystuff', {template: 'partials/MyStuff.html', controller: MyStuffController});
-    $routeProvider.when('/mystuff/:id', {template: 'partials/MyStuffEdit.html', controller: MyStuffEditController});
+    $routeProvider.when('/mystuff', {template: 'partials/my-stuff.html', controller: MyStuffController});
+    $routeProvider.when('/mystuff/:id', {template: 'partials/my-stuff-edit.html', controller: MyStuffEditController});
     $routeProvider.when('/friendsstuff', {template: 'partials/partial2.html', controller: MyCtrl2});
     $routeProvider.when('/friends', {template: 'partials/friends.html', controller: FriendsController});
     $routeProvider.when('/friends/:id', {template: 'partials/friend-edit.html', controller: FriendEditController});
