42cb2e33239edd32a881b247b1cce17ed26ed898
shybyte <ein@volloeko.de>
2012-04-26 12:59:36 +0200

    Updated build scripts to use LABjs. Removed index-dev.html

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,13 +6,18 @@ LIB=$SOURCE/lib
 echo "Make Dirs ..."
 mkdir $TARGET
 mkdir $TARGET/js
-mkdir $TARGET/css
+#mkdir $TARGET/lib
+#mkdir $TARGET/lib/labjs
 
 echo "Copy stuff..."
+cp -r $SOURCE/css $TARGET
 cp -r $SOURCE/img $TARGET
 cp    $LIB/bootstrap/img/* $TARGET/img
 cp -r $SOURCE/partials $TARGET
 cp $SOURCE/index.html $TARGET
+cp -r $LIB  $TARGET
+#cp $LIB/labjs/LAB.js  $TARGET/lib/labjs
+#cp -r $LIB/bootstrap  $TARGET/lib
 
 echo "Compress javascripts..."
 JS=$SOURCE/js
@@ -20,6 +25,13 @@ JSG=$SOURCE/js/gen
 java -jar $BASE_DIR/buildtools/closure-compiler.jar $JSG/utils.js $JS/remote-storage-utils.js $JSG/models.js $JSG/services.js \
  $JSG/app-controllers.js $JSG/friends-controllers.js $JSG/my-stuff-controllers.js $JSG/friends-stuff-controllers.js  $JS/controllers.js \
  $JS/directives.js $JS/filters.js $JSG/account-controllers.js $JS/app.js --js_output_file $TARGET/js/shared-stuff.min.js
+
+#cat $JSG/utils.js $JS/remote-storage-utils.js $JSG/models.js $JSG/services.js \
+# $JSG/app-controllers.js $JSG/friends-controllers.js $JSG/my-stuff-controllers.js $JSG/friends-stuff-controllers.js  $JS/controllers.js \
+# $JS/directives.js $JS/filters.js $JSG/account-controllers.js $JS/app.js >$TARGET/js/shared-stuff.min.js
+
+echo "Combine local libs ..."
+cat $LIB/underscore.min.js $LIB/angular/angular.min.js $LIB/bootstrap/js/bootstrap.min.js $LIB/remoteStorage.js >$TARGET/js/local-libs.min.js
 
 echo "Combine javascripts..."
 cat $LIB/jquery.min.js $LIB/underscore.min.js $LIB/angular/angular.min.js $LIB/bootstrap/js/bootstrap.min.js $LIB/remoteStorage.js $TARGET/js/shared-stuff.min.js >$TARGET/js/shared-stuff-with-libs.min.js
